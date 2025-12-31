98b3ddb23c71c7a4feea1bb191795a7b6f41ac71
Andres Pineda <andres.pineda@allsystems.com>
2025-12-30 23:40:34 -0500

    changes to the favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <x-google-analytics />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon.ico') }}">
 
     <!-- Title -->
     <title>LaravelHelp - Expert Laravel Consulting & Audits</title>
